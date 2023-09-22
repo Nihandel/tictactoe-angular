2b393ea780f898496f9d62af0731a1225ed07f43
francesco finotto <finottofrancesco1@gmail.com>
2023-09-22 15:33:59 +0200

    basic logic of the game

--- tictactoeAngular/src/app/pages/home-page/home-page.component.ts
+++ tictactoeAngular/src/app/pages/home-page/home-page.component.ts
@@ -1,19 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
-import { MatchService } from 'src/app/services/match.service';
+import { Observable, tap } from 'rxjs';
+import { MatchService, TTTButtonState } from 'src/app/services/match.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
   standalone: true,
-  imports:[MatButtonModule,MatListModule]
+  imports:[CommonModule,MatButtonModule,MatListModule],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
   headerContent: String = "header";
-
+  buttons: Array<boolean|undefined>;
+  currentPlayer:TTTButtonState = TTTButtonState.X;
+  tableState$:Observable<Array<TTTButtonState|undefined|null>>;
   constructor(private match: MatchService) {
-    
+    this.buttons = new Array(9);
+    this.tableState$ = match.$table.pipe(tap((value) => {
+       console.log(value) 
+      }));
+    this.match.matchStatus$.subscribe((value)=>{
+      let message:string;
+      if(value){
+        alert(value.toString());
+      }
+    });
+  }
+  ngOnInit(): void {
+    this.match.clearTable();
+  }
+  newMatch(){
+    this.match.clearTable();
+  }
+  onButtonClick(index:number,player:TTTButtonState):void{
+    console.log(`clicked on ${index} for player ${player}`);
+    this.match.makeMove(index,player);
+    this.togglePlayer();
+  }
+  private togglePlayer(){
+    this.currentPlayer = this.currentPlayer == TTTButtonState.X ? TTTButtonState.O : TTTButtonState.X;
   }
 }
--- tictactoeAngular/src/app/services/match.service.ts
+++ tictactoeAngular/src/app/services/match.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
+export enum TTTButtonState {
+  X = "X", O = "O",
+}
+export enum TTTMatchResult {
+  WinX="Winner: X",
+  WinO="Winner: O",
+  Draw="Draw",
+}
 @Injectable({
   providedIn: 'root'
 })
 export class MatchService {
+  public get $table(): Observable<Array<TTTButtonState | undefined | null>> {
+    return this._stream.asObservable();
+  }
+  public get matchStatus$(): Observable<TTTMatchResult | undefined> {
+    return this._matchStatus.asObservable();
+  }
+  constructor() {
+    this.clearTable();
+  }
+  makeMove(index: number, player: TTTButtonState) {
+    var temp = this.table;
+    temp[index] = player;
+    this.table = temp;
+    const result = this.checkWinner();
+    if (!!result) {
+      // winner
+      this._matchStatus.next(result == TTTButtonState.X ? TTTMatchResult.WinX : TTTMatchResult.WinO);
+    } else if (this._moveLeft <= 0) {
+      // draw
+      this._matchStatus.next(TTTMatchResult.Draw);
+    }
+  }
+  clearTable() {
+    this.table = new Array<TTTButtonState | undefined | null>(9).fill(null);
+    this._moveLeft = 9;
+  }
 
-  constructor() { }
+  checkWinner(): TTTButtonState | undefined | null {
+    const victoryConditions: Array<Array<number>> = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < victoryConditions.length; i++) {
+      let condition = victoryConditions[i];
+      let a = this._table[condition[0]];
+      let b = this._table[condition[1]];
+      let c = this._table[condition[2]];
+      if (!!a && a == b && b == c) {
+        return a;
+      }
+    }
+    return null;
+  }
+  private set table(value: Array<TTTButtonState | undefined | null>) {
+    this._table = value;
+    this._stream.next(this.table);
+  }
+  private get table(): Array<TTTButtonState | undefined | null> {
+    return [...this._table];
+  }
+  private _moveLeft!: number;
+  private _stream: BehaviorSubject<Array<TTTButtonState | undefined | null>> = new BehaviorSubject<Array<TTTButtonState | undefined | null>>([]);
+  private _matchStatus: BehaviorSubject<TTTMatchResult | undefined> = new BehaviorSubject<TTTMatchResult | undefined>(undefined);
+  private _table!: Array<TTTButtonState | undefined | null>;
 }
